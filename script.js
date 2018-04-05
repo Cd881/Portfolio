85a30db46020c3034499a70ef3968d02870a896b
Dauer Cameron <310042881@cart.ads>
2018-04-05 15:31:21 -0700

    doneish

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var console,
-    document,
+var document,
     setInterval,
     clearInterval,
     x = 0,
@@ -10,7 +9,6 @@ var console,
 function dropdown() {
     'use strict';
     var elem = document.getElementById("dropdown");
-    
     function down() {
         if (x === 0) {
             clearInterval(a);
@@ -19,7 +17,6 @@ function dropdown() {
             elem.style.top = x + "px";
         }
     }
-        
     if (x === -80) {
         a = setInterval(down, 1);
     }
@@ -28,7 +25,6 @@ function dropdown() {
 function dropup() {
     'use strict';
     var elem = document.getElementById("dropdown");
-    
     function up() {
         if (x === -80) {
             clearInterval(b);
@@ -37,7 +33,6 @@ function dropup() {
             elem.style.top = x + "px";
         }
     }
-    
     if (x === 0) {
         b = setInterval(up, 1);
     }
@@ -53,7 +48,6 @@ function fadeOut() {
     var doc = document.getElementById('overlay'),
         a = 0.7,
         x;
-    
     function fade() {
         if (a === 0.2) {
             clearInterval(x);
@@ -69,8 +63,8 @@ function quote(num) {
     'use strict';
     var doc1 = document.getElementById("q1"),
         doc2 = document.getElementById("q2"),
-        doc3 = document.getElementById("q3");
-    
+        doc3 = document.getElementById("q3"),
+        z = 0;
     if (num === -1 && slides === 1) {
 //        console.log("equal 3");
         slides = 3;
@@ -88,19 +82,17 @@ function quote(num) {
         slides -= 1;
 //        console.log(slides);
     }
-    
     if (slides === 1) {
-        doc1.style.left = 0 + "px";
+        doc1.style.left = z + "px";
         doc2.style.left = -100 + "%";
         doc3.style.left = -100 + "%";
     } else if (slides === 2) {
         doc1.style.left = -100 + "%";
-        doc2.style.left = 0 + "px";
+        doc2.style.left = z + "px";
         doc3.style.left = -100 + "%";
     } else if (slides === 3) {
         doc1.style.left = -100 + "%";
         doc2.style.left = -100 + "%";
-        doc3.style.left = 0 + "px";
+        doc3.style.left = z + "px";
     }
-    
 }
